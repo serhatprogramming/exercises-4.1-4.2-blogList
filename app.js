f282e028fe06026b0698c87c806dbf12f30dd1aa
serhatprogramming <serhatprogramming@gmail.com>
2023-03-21 14:46:23 -0400

    middleware errorhandler added

--- app.js
+++ app.js
@@ -17,5 +17,7 @@ app.use(express.json());
 
 app.use(middleware.requestLogger);
 app.use("/api/blogs", blogsRouter);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -7,12 +7,15 @@ blogsRouter.get("/", (request, response) => {
   });
 });
 
-blogsRouter.post("/", (request, response) => {
+blogsRouter.post("/", (request, response, next) => {
   const blog = new Blog(request.body);
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
+  blog
+    .save()
+    .then((result) => {
+      response.status(201).json(result);
+    })
+    .catch((e) => next(e));
 });
 
 module.exports = blogsRouter;
