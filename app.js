ad31a9d2d01cadf7e3f4f1de1cad973bc7773340
serhatprogramming <serhatprogramming@gmail.com>
2023-03-21 14:17:02 -0400

    refactor models controllers

--- app.js
+++ app.js
@@ -5,15 +5,8 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
+const Blog = require("./models/blog");
+const blogsRouter = require("./controllers/blogs");
 
 const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl);
@@ -21,18 +14,6 @@ mongoose.connect(mongoUrl);
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use("/api/blogs", blogsRouter);
 
 module.exports = app;
